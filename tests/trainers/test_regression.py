a850a4424824bbfa72dac1e2845ebd8a0aedb0b2
Adam J. Stewart <ajstewart426@gmail.com>
2023-01-05 21:14:44 -0600

    Simplify validation plotting

--- tests/trainers/test_regression.py
+++ tests/trainers/test_regression.py
@@ -60,25 +60,6 @@ class TestRegressionTask:
         except MisconfigurationException:
             pass
 
-    def test_no_logger(self) -> None:
-        conf = OmegaConf.load(os.path.join("tests", "conf", "cyclone.yaml"))
-        conf_dict = OmegaConf.to_object(conf.experiment)
-        conf_dict = cast(Dict[Any, Dict[Any, Any]], conf_dict)
-
-        # Instantiate datamodule
-        datamodule_kwargs = conf_dict["datamodule"]
-        datamodule = TropicalCycloneDataModule(**datamodule_kwargs)
-
-        # Instantiate model
-        model_kwargs = conf_dict["module"]
-        model = RegressionTask(**model_kwargs)
-
-        # Instantiate trainer
-        trainer = Trainer(
-            logger=False, fast_dev_run=True, log_every_n_steps=1, max_epochs=1
-        )
-        trainer.fit(model=model, datamodule=datamodule)
-
     @pytest.fixture
     def model_kwargs(self) -> Dict[Any, Any]:
         return {
